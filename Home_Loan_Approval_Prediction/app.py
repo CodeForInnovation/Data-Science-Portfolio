a7a04cf9c050eff1396ca3612cf14e5efdae151e
CodeForInnovation <68452748+CodeForInnovation@users.noreply.github.com>
2024-07-09 18:13:44 +0530

    Add files via upload

--- Home_Loan_Approval_Prediction/app.py
+++ Home_Loan_Approval_Prediction/app.py
@@ -1,70 +1,68 @@
 import streamlit as st
-import joblib
 import pandas as pd
-import datetime
-import json
-from big_query import BigQuery
+from youtube_data import TrendingVideos
 
 
 
-st.title("Welcome to ABC Bank")
 
-model = joblib.load('model_final.joblib')
-#Even though we are not going to use gender to predict the loan status,
-#we will be getting the gender data for future plans/schemes.
-with st.form('Loan Form',clear_on_submit=True):
-    col1,col2 = st.columns(2)
-    with col1:
-        Gender = st.selectbox('Gender',('Male','Female'))
-        Applicant_Income = st.number_input('Applicant Income',min_value=15000)
-        Coapplicant_Income = st.number_input('Co-applicant Income',min_value=0)
-        Loan_amount = st.number_input('Loan Amount (In Lakhs)',min_value=2)
-        Loan_Amount_Term = st.number_input('Loan Amount Term (Months)',min_value=12)
-    with col2:
-        Property_Area = st.selectbox('Property Area',('Urban','Rural','Semiurban'))
-        Credit_History = st.number_input('Credit History',min_value=0,max_value=1)
-        Self_Employed = st.selectbox('Self Employed',('Yes','No'))
-        Dependents = st.selectbox('Dependents',('0','1','2','3+'))
-        Education = st.selectbox('Education',('Graduate','Not Graduate'))
-        Married = st.selectbox('Married',('Yes','No'))
+#Using cache data to avoid calling the api multiple times in a session
+@st.cache_data
+def fetch_get_video(region_code,cat):
+    video = TrendingVideos(cat)
+    return video.get_video(region_code)
 
-    df = pd.DataFrame({
-        'Married': [Married],
-        'Dependents': [Dependents],
-        'Education': [Education],
-        'Self_Employed': [Self_Employed],
-        'Applicant_Income': [Applicant_Income/100],
-        'Coapplicant_Income': [Coapplicant_Income/100],
-        'Loan_Amount': [Loan_amount],
-        'Loan_Amount_Term': [Loan_Amount_Term],
-        'Credit_History': [Credit_History],
-        'Property_Area': [Property_Area]}
-        )
-    def emi_calculator(principle,term):
-        #interest = PNR/100
-        interest = (principle * 1000 * 8.5 * term )/float(12*100)
-        emi = ((principle*1000) + interest)/term
-        return emi
-    df['EMI'] = df.apply(lambda row: emi_calculator(row['Loan_Amount'],row['Loan_Amount_Term']),axis =1)
-    df['EMI'] = round(df['EMI'],2)
-    df['Balance_Income'] = (df['Applicant_Income'] + df['Coapplicant_Income']) - df['EMI']
-    df['Balance_Income'] = round(df['Balance_Income'],2)
-    
+#Using cache resource to save the data permanently after building app
+@st.cache_resource
+def get_cat():
+    cat =pd.read_csv('category.csv',index_col ='id')
+    return cat
+
+cat = get_cat()
+
+#Let's map some of ISO 3166-1 alpha-2 country code with country names to use in the web app
+@st.cache_resource
+def get_location():
+    return pd.Series(data=['INDIA','USA','CANADA','AUSTRALIA','SOUTH AFRICA'],
+                              index =['IN','US','CA','AU','ZA'])
+
+location = get_location()
+#creating a function to return like/view counts as k(kilo) and M(million)
+def transform(counts):
+    if counts < 1000:
+        return counts
+    elif counts < 1000000:
+        return f"{round(counts/1000,1)}k"
+    else:
+        return f"{round(counts/1000000,1)}M"
 
 
-    submit = st.form_submit_button('Predict')
-    if submit:
-        prediction = model.predict(df.drop(columns = ['Applicant_Income','Coapplicant_Income','Loan_Amount','Loan_Amount_Term']))
-        df['Gender'] = Gender
-        df['_created_at'] = datetime.datetime.now()
-        df['_created_by'] = 'user'
-        df['Approval_Id'] = df['_created_by']+'-'+df['_created_at'].astype('str')
-        df['Prediction'] = prediction
-        bg=BigQuery()
-        bg.load_data(df)
-        if prediction:
-    
-            st.success(f'Congratulations, Your Home Loan is Approved!!')
-        else:
-            st.error('We are extremely sorry to inform you that you Home Loan is not approved. Please reach out to nearest branch for further clarification')
+
+st.title('Trending YouTube Videos')
+region = st.sidebar.radio('Select Region',['Global','INDIA','USA','CANADA','AUSTRALIA','SOUTH AFRICA'])
+category_filter = st.sidebar.multiselect('Filter by Category',cat['title'].tolist())
+sort_by = (st.sidebar.radio('Sort by',['Trend','Date','Likes','Views']))
+
+#fetching the trending videos based on region code and to get the global trend, we need to pass None
+trending_videos = fetch_get_video(location.index[location == region][0] if region != 'Global' else None,cat)
+
+
+filter_videos = trending_videos.copy()
+
+if category_filter:
+    filter_videos = filter_videos[filter_videos['category'].isin(category_filter)]
+
+if sort_by !='Trend':
+    filter_videos = filter_videos.sort_values(by=sort_by.lower(),ascending=False)
+
+
+
+col1,col2= st.columns([2,1])
+# just to contain the output in a particular width, creating 2 columns with 1st column twice in width as second column
+with col1:
+    for i,video in filter_videos.iterrows():
+        st.image(video['thumbnail'],caption=f" {transform(video['views'])} Views  ❤️ {transform(video['likes'])}  ",width=450)
+        st.write(f"[{video['title']}]({video['url']})")
+
+
+
 
