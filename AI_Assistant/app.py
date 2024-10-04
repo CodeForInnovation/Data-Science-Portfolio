17170c30494951b843bc25af5d9f601e06439e03
CodeForInnovation <68452748+CodeForInnovation@users.noreply.github.com>
2024-10-04 12:01:02 +0530

    Add files via upload

--- AI_Assistant/app.py
+++ AI_Assistant/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from gemini import Model
+from PIL import Image
 # Function to simulate model responses
 
 #using cache resource to inference the model once and use it for all the sessions
@@ -27,13 +28,24 @@ for message in st.session_state.messages:
     with st.chat_message(message['role']):
         st.markdown(message['content'])
 
+prompt = []
+col1, col2 = st.columns([2,4])
+with col1:
+    file = st.file_uploader('',type=["jpg", "jpeg", "png"])
+    if file is not None:
+        file_input = Image.open(file)
+        prompt.append(file_input)
+
 
 #displaying user's current interaction
-if prompt := st.chat_input('How can I help you?'):
-    with st.chat_message('user'):
-        st.markdown(prompt)
-    st.session_state['messages'].append({'role':'user','content':prompt})
-    response = model.get_response(prompt)
-    with st.chat_message('assistant'):
-        st.markdown(response)
-    st.session_state['messages'].append({'role':'assistant','content':response})
+
+with col2:
+    if user_input := st.chat_input('How can I help you?',):
+        prompt.append(user_input)
+        with st.chat_message('user'):
+            st.markdown(prompt)
+        st.session_state['messages'].append({'role':'user','content':prompt})
+        response = model.get_response(prompt)
+        with st.chat_message('assistant'):
+            st.markdown(response)
+        st.session_state['messages'].append({'role':'assistant','content':response})
